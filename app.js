0aec03cab91b4b05952daf3f6f00c26931ec14d2
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-02-01 09:02:06 +0200

    add the arch

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ async function main() {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var wikiRouter = require('./routes/wiki');
+
 
 var app = express();
 
@@ -30,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wiki', wikiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
